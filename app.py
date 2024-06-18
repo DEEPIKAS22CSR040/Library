6fc89ed94996af7d58d9e7197194001534614c81
DEEPIKAS22CSR040 <127317696+DEEPIKAS22CSR040@users.noreply.github.com>
2024-06-18 12:04:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,34 @@ def admin():
 def student():
     return render_template('student.html')
 
+@app.route('/bp/<bid>', methods=['GET', 'POST'])
+def bp(bid):
+    if request.method == 'POST':
+        user_id = request.form.get('user_id')
+        borrow_date = request.form.get('borrow_date')
+        return_date = request.form.get('return_date')
+
+        try:
+            mycursor = mydb.cursor()
+            mycursor.execute("""
+                INSERT INTO borrowing (user_id, book_id, borrow_date, return_date)
+                VALUES (%s, %s, %s, %s)
+            """, (user_id, bid, borrow_date, return_date))
+            mycursor.execute("""
+                UPDATE book SET available = FALSE WHERE bid = %s
+            """, (bid,))
+            mydb.commit()
+            flash('Book borrowed successfully!')
+        except Exception as e:
+            mydb.rollback()
+            flash(f"Error borrowing book: {e}")
+        finally:
+            mycursor.close()
+
+        return redirect(url_for('table1'))
+    else:
+        return render_template('bp.html', bid=bid)
+
 @app.route('/student1', methods=['POST', 'GET'])
 def student1():
     if request.method == 'POST':
@@ -246,6 +274,97 @@ def delete(id):
         if mycursor:
             mycursor.close()
     return redirect('/table')
+
+
+@app.route('/view_borrowed_books', methods=['GET'])
+def view_borrowed_books():
+    mycursor = None
+    borrowed_books = []
+    try:
+        mycursor = mydb.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        mycursor.execute("""
+            SELECT b.bid, b.bname, b.bcategory, b.language, b.byear, a.author_name, a.description,
+                   u.user_id, u.username
+            FROM book b
+            LEFT JOIN author a ON b.bid = a.bid
+            LEFT JOIN borrowing br ON b.bid = br.book_id
+            LEFT JOIN users u ON br.user_id = u.user_id
+            WHERE br.returned = FALSE
+        """)
+        borrowed = mycursor.fetchall()
+        for book in borrowed:
+            borrowed_book = {
+                'bid': book['bid'],
+                'bname': book['bname'],
+                'bcategory': book['bcategory'],
+                'language': book['language'],
+                'byear': book['byear'],
+                'author_name': book['author_name'],
+                'description': book['description'],
+                'user_id': book['user_id'],
+                'username': book['username']
+            }
+            borrowed_books.append(borrowed_book)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        if mycursor:
+            mycursor.close()
+    return render_template('view_borrowed_books.html', books=borrowed_books)
+
+
+@app.route('/view_available_books', methods=['GET'])
+def view_available_books():
+    mycursor = None
+    available_books = []
+    try:
+        mycursor = mydb.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        mycursor.execute("""
+            SELECT b.bid, b.bname, b.bcategory, b.language, b.byear, a.author_name, a.description
+            FROM book b
+            LEFT JOIN author a ON b.bid = a.bid
+            WHERE b.available = TRUE
+        """)
+        available = mycursor.fetchall()
+        for book in available:
+            available_book = {
+                'bid': book['bid'],
+                'bname': book['bname'],
+                'bcategory': book['bcategory'],
+                'language': book['language'],
+                'byear': book['byear'],
+                'author_name': book['author_name'],
+                'description': book['description'],
+            }
+            available_books.append(available_book)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        if mycursor:
+            mycursor.close()
+    return render_template('view_available_books.html', books=available_books)
+
+@app.route('/search_books', methods=['GET'])
+def search_books():
+    mycursor = None
+    category = request.args.get('category', '')
+
+    try:
+        mycursor = mydb.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        mycursor.execute("""
+            SELECT b.bid, b.bname, b.bcategory, b.language, b.byear, a.author_name, a.description 
+            FROM book b
+            LEFT JOIN author a ON b.bid = a.bid 
+            WHERE b.bcategory = %s
+        """, (category,))
+        books = mycursor.fetchall()
+        return render_template('search_results.html', books=books, category=category)
+    except psycopg2.Error as e:
+        print("Error searching books:", e)
+        return render_template('search_results.html', books=[], category=category)
+    finally:
+        if mycursor:
+            mycursor.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
